9cc3dcd1d7fc7921499da73e15f07d153121346e
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2024-05-02 17:26:15 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -3,65 +3,25 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false); //런타임 에러 해결
     cin.tie(NULL);
     cout.tie(NULL);
 
     int N, M;
     cin >> N >> M;
-
-    // //주어진 값들 배열화
-    // int nums[100000];
-    // for(int i=0; i<N; i++){
-    //     cin >> nums[i];
-    // }
   
     //구간합 배열화
-    int sum_nums[100001] = {};
-    // sum_nums[0] = nums[0]; //1번째 인덱스에 초기 값 할당
+    int sum_nums[100001] = {}; //모든 배열 요소 0으로 초기화
     for (int i=1; i<=N; i++){
         int num;
         cin >> num;
         sum_nums[i] = num + sum_nums[i-1];
-        // cout << sum_nums[i];
     }
-
-    // // //M개의 구간합 구하기
-    // string line;
-    // int cnt = 0;
-    // getline(cin, line);
-    // for(int j=0; j<M; j++){
-    //     getline(cin, line); // 라인단위로 읽기. \n이 나올때까지
-    //     stringstream ss(line);
-
-    //     string buffer;
-    //     int start, end;
-    //     while (getline(ss, buffer, ' ')){
-    //         if(cnt == 0){
-    //             cout << "start : " << buffer << endl;
-    //             // start = atoi(buffer.c_str());
-    //             cnt++;
-    //         }
-    //         else{
-    //             cout << "end : " << buffer << endl;
-    //             // end = atoi(buffer.c_str());
-    //         }
-    //     }
-    //     // cout << sum_nums[end] - sum_nums[start] << endl;
-        
-    //     cnt = 0;
-    //     // int start, end;
-    //     // cin >> start >> end; 
-    //     // // cin >> end;
-    //     // cout << start;
-    //     // cout << endl;
-    //     // // cout << sum_nums[end] - sum_nums[start-1] << endl;
-    // }
 
     for(int j=0; j<M; j++){
         int start, end;
         cin >> start >> end;
-        cout << sum_nums[end] - sum_nums[start-1] << "\n"; //인덱스가 0번부터 시작하므로 i번째 원소는 i-1번째 인덱스를 말한다
+        cout << sum_nums[end] - sum_nums[start-1] << "\n"; //모든 배열이 0으로 초기화 되어있으므로 극한값. 예를 들어 1번째~1번째 구간합을 구할때 상관없어짐
     }
 
     return 0;
