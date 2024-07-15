ac7af024ba18e4bdd438aa6e7c54f46271a1de70
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2024-07-15 16:00:02 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    // int nums[5];
+    int N;
+    cin >> N;
+    vector<int> nums(N,0);
+
+    for(int i=0; i<N; i++){
+        cin >> nums[i];
+    }
+
+    for(int i=0; i<N-1; i++){
+        for(int j=0; j<N-i-1; j++){
+            if(nums[j] > nums[j+1]){
+                // int replace_num = nums[j+1];
+                int replace_num = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = replace_num;
+                // nums[j+1] = nums[j];
+                // nums[j] = replace_num;
+            }
+            // cout << "j : " << j;
+        }
+    }
+
+    for(int i=0; i<N; i++){
+        cout << nums[i] << "\n";
+    }
+
+    return 0;
+}
