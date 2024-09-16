e3fa39c1ce3b50033d49ca2d2b8533797005e16a
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2024-09-16 13:26:42 +0900

    [level 1] Title: 하샤드 수, Time: 8.11 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -2,11 +2,24 @@ class Solution {
     public boolean solution(int x) {
         String num_str = "" + x;
         int result = 0;
-        for(int i=0; i<num_str.length(); i++){
-            int num = Character.getNumericValue(num_str.charAt(i));
-            result += num;
+        // for(int i=0; i<num_str.length(); i++){
+        //     int num = Character.getNumericValue(num_str.charAt(i));
+        //     result += num;
+        // }
+        int new_x = x;
+        while(new_x>0){
+            // System.out.println(x/10);
+            // System.out.println(x%10);
+
+            // System.out.println(x);
+
+            result += (new_x%10);
+            // System.out.println(result);
+
+            new_x/=10;
         }
-        
+        // result+=x;
+        System.out.println(result);
         return x % result == 0;
     }
 }
