ecae5381fd94f7ad40cc80e2271223cff95526a3
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2024-09-19 01:34:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -2,13 +2,9 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         for(int i=0; i<absolutes.length; i++){
-            int n = (signs[i] == false)? absolutes[i] * -1 : absolutes[i];
-            // if(signs[i] == false){
-            //     n = absolutes[i] * -1;
-            // }else{
-            //     n = absoulte
-            // }   
-            answer += n;
+            // int n = (signs[i] == false)? absolutes[i] * -1 : absolutes[i];
+            // answer += n;
+            answer += (signs[i] == false)? absolutes[i] * -1 : absolutes[i];
         }
         return answer;
     }
