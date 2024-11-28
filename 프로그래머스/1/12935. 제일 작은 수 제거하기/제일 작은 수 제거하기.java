a6380ee36b16aeba6f8818f55663ec1baaf1b25b
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2024-11-28 13:40:53 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.45 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        if(arr.length ==1){
+            int[] empty = new int[]{-1};
+            return empty;
+        }
+        
+        int[] answer = new int[arr.length-1];
+        int min_val = arr[0];
+        int idx = 0;
+        for(int i=1; i<arr.length; i++){
+            if(min_val > arr[i]){
+                min_val = arr[i];
+                idx = i;
+            }
+        }
+        
+        // for(int i=0; i<arr.length-1; i++){
+        //     if(idx == i){
+        //         continue;
+        //     }
+        //     answer[i] = arr[i];
+        // }
+        
+        int j = 0; // answer 배열의 인덱스
+        for (int i = 0; i < arr.length; i++) {
+            if (i != idx) { // 최소값의 인덱스는 건너뛴다
+                answer[j++] = arr[i];
+            }
+        }
+
+        return answer;
+    }
+}
