0597f0f484c3b9a677744cf06d667e0c7e473ed3
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2024-12-27 01:02:47 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.34 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = Arrays.stream(numbers)
+                             .map(n->n*2)
+                             .toArray();
+        return answer;
+    }
+}
