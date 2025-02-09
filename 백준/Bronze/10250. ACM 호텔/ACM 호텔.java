5e5d661c94494f7044366d1439ae3848ac69a333
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-02-09 17:29:00 +0900

    [Bronze III] Title: ACM 호텔, Time: 116 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -17,7 +17,15 @@ public class Main {
 
             int roomNum = (N%H == 0)? N/H : N/H + 1;
             int floor = (N%H == 0)? H : N%H;
-            
+
+            if(N%H == 0){
+                roomNum = N/H;
+                floor = H;
+            }else{
+                roomNum = N/H + 1;
+                floor = N%H;
+            }
+
             sb.append(floor*100 + roomNum).append("\n");
         }
         System.out.print(sb.toString());
