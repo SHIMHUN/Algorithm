b820a87a8e326b284719b0d45226013f48843cf1
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-02-09 17:27:52 +0900

    [Bronze III] Title: ACM 호텔, Time: 116 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb =  new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++){
+            String[] s = br.readLine().split(" ");
+            int[] input = Arrays.stream(s).mapToInt(Integer::parseInt).toArray();
+            int H = input[0];
+            int W = input[1];
+            int N = input[2];
+
+            int roomNum = (N%H == 0)? N/H : N/H + 1;
+            int floor = (N%H == 0)? H : N%H;
+            
+            sb.append(floor*100 + roomNum).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
