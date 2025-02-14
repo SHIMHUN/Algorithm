e019c7ced2856d9a46ac88edfff358dc173ef31b
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-02-14 16:26:58 +0900

    [Gold V] Title: 4와 7, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Gold/2877. 4와 7/4와 7.java
+++ 백준/Gold/2877. 4와 7/4와 7.java
@@ -1,26 +1,29 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int K;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int K = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
 
-        System.out.println(findLuckyNumber(K));
+        System.out.println(findNum());
     }
 
-    public static String findLuckyNumber(int K) {
-        StringBuilder sb = new StringBuilder();
+    public static String findNum() {
 
-        // K를 2진수로 변환 (1-based index)
-        K = K + 1;  // 1부터 시작하는 인덱스로 맞추기
+        K = K + 1;
 
         while (K > 1) {
-            if (K % 2 == 0) sb.append("4"); // 짝수이면 4
-            else sb.append("7");  // 홀수이면 7
+            if (K % 2 == 0) {
+                sb.append("4"); // 짝수이면 4
+            } else{
+                sb.append("7");  // 홀수이면 7
+            }
             K /= 2;
         }
 
-        return sb.reverse().toString(); // 앞에서부터 만든 값을 뒤집기
+        return sb.reverse().toString(); // 결과를 뒤집어서 출력 -> 이진수는 뒤부터 시작됨
     }
 }
