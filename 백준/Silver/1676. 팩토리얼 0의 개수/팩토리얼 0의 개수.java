2329ca2c361e7720d302f0d720dbe181726e6f75
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-02-28 00:23:01 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 108 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -4,29 +4,23 @@ import java.io.*;
 
 public class Main {
 //    static Long sum;
+    static BigInteger factorial;
     static int N, cnt;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
 
-//        sum=1L;
-
-        BigInteger factorial = BigInteger.ONE;
+        factorial = BigInteger.ONE;
 
         for(int i=1; i<=N; i++){
             factorial = factorial.multiply(BigInteger.valueOf(i));
-//            sum *= i;
         }
 
         String s = factorial.toString();
 
-//        String s = Long.toString(sum);
         cnt=0;
 
-
-//        System.out.println(s);
         for(int i=s.length()-1; i>0; i--){
             if(s.charAt(i) == '0'){
                 cnt++;
