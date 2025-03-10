ad3eec93230f234927850c72d954477a2ea01af9
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-03-10 19:48:43 +0900

    [Silver V] Title: 나이순 정렬, Time: 540 ms, Memory: 52488 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -3,25 +3,26 @@ import java.io.*;
 
 public class Main {
     static Map<Integer, List<String>> group = new HashMap<>();
+    static int N;
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
         for(int i=0; i<N; i++){
-            String[] member_str = br.readLine().split(" ");
-            int age = Integer.parseInt(member_str[0]);
-            String member = member_str[1];
+            st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String member = st.nextToken();
+//            String[] member_str = br.readLine().split(" ");
+//            int age = Integer.parseInt(member_str[0]);
+//            String member = member_str[1];
 
             group.putIfAbsent(age, new ArrayList<>());
             group.get(age).add(member);
         }
-
-//        for(int i=0; i<group.size(); i++){
-//
-//        }
 
         for(Map.Entry<Integer, List<String>> entry : group.entrySet()){
             List<String> value = entry.getValue();
