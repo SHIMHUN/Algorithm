e38b732b18c3cddbe138cb621b8bb1222e657944
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-05-07 17:57:48 +0900

    [level 1] Title: 콜라 문제, Time: 0.15 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,7 +2,17 @@ class Solution {
     static int cnt = 0;
     
     public int solution(int a, int b, int n) {
-        recur(a,b,n);
+        // //1. 재귀함수 이용
+        // recur(a,b,n);
+        
+        //2. 일반 반복문 이용
+        while (n >= a) { // 남은 병이 a보다 크거나 같을 동안 계속 교환을 반복
+            int x = n / a * b; 
+            int y = n % a;
+            cnt += x; 
+            n = x + y; 
+        }
+        
         return cnt;
     }
     
@@ -15,7 +25,6 @@ class Solution {
         int y = n % a; //나머지
         cnt+=x;
         
-        System.out.println(x + " " + y);
         recur(a, b, x+y);
     }
 }
