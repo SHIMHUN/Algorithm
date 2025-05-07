171a3db52f6e22c0135a0d9185e6d5c6c5d31c3a
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-05-07 17:22:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 3.75 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,22 +1,28 @@
 class Solution {
     public String solution(int[] food) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
         
         //1. 1번 인덱스부터 배열 돌면서 2로 나눈 몫 구하기
         for(int i=1; i<food.length; i++){
             int loop = food[i]/2;
-            for(int j=0; j<loop; j++){
-                answer+=i;
-            }
+            sb.append(String.valueOf(i).repeat(loop));
+            // for(int j=0; j<loop; j++){
+            //     answer+=i;
+            // }
         }
         
-        //2. 결과 문자열 뒤집기
-        StringBuilder sb = new StringBuilder(answer);
-        String reversed = sb.reverse().toString();
+        answer = sb + "0";
+        answer += sb.reverse().toString();
+        // answer = sb.toString();
         
-        //3. 첫 결과 문자열에 0 추가하고 뒤집은 문자열 붙이기
-        answer+='0';
-        answer+=reversed;
+//         //2. 결과 문자열 뒤집기
+//         StringBuilder sb = new StringBuilder(answer);
+//         String reversed = sb.reverse().toString();
+        
+//         //3. 첫 결과 문자열에 0 추가하고 뒤집은 문자열 붙이기
+//         answer+='0';
+//         answer+=reversed;
         
         return answer;
     }
