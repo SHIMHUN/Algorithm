647fdd3b4b9190518014b96fc9b8e026024da447
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-05-08 18:04:01 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.38 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,32 +1,22 @@
-import java.util.*;
-
 class Solution {
-    static Map<Character, PriorityQueue<String>> map = new TreeMap<>();
-    static ArrayList<String> al = new ArrayList<>();
-    
-    public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length];
-        
-        for(int i=0; i<strings.length; i++){
-            //key에 해당하는 값이 없으면 우선순위 큐 새로 생성해서 삽입
-            map.putIfAbsent(strings[i].charAt(n), new PriorityQueue<>());
-            
-            //key에 해당하는 value로 큐를 가져와서 문자열 삽입
-            map.get(strings[i].charAt(n)).offer(strings[i]);
-        }
-        
-        //value값만 탐색
-        for(PriorityQueue<String> pq : map.values()){
-            
-            //큐의 첫번째 값을 제거하고 반환하여 결과 배열에 추가
-            while(!pq.isEmpty()){
-                al.add(pq.poll());
-            }
-        }
-        
-        //ArrayList를 String[] 배열로 변환
-        al.toArray(answer);
-        
-        return answer;
-    }
+  public String[] solution(String[] strings, int n) {
+
+      for(int i=0; i<strings.length-1; i++){
+          for(int k=i+1; k<strings.length;k++){
+              if(strings[i].charAt(n)>strings[k].charAt(n)){ 
+                  String tmp = strings[i];
+                  strings[i]= strings[k];
+                  strings[k]=tmp;
+              }
+              else if(strings[i].charAt(n)==strings[k].charAt(n)){
+                  if(strings[i].compareTo(strings[k]) >0){ 
+                      String tmp = strings[i];
+                      strings[i]= strings[k];
+                      strings[k]=tmp;
+                  }
+              }
+          }
+      }
+      return strings;
+  }
 }
