75d9598c29cdc39752b8603d6f53adfbb983ae42
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-05-27 00:02:10 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 8.55 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+        
+        int cnt=0;
+        int score=0;
+        
+        for(int num : lottos){
+            if(num == 0){ //0인 경우
+                cnt++;
+            }else{
+                for(int win_num : win_nums){ 
+                    if(num == win_num){ //번호 맞춘 경우
+                        score++;
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        int max_score = score + cnt;
+        int min_score = score;
+        
+        System.out.println("max_score: " + max_score);
+        System.out.println("min_score: " + min_score);
+        
+        // answer[0] = 6-max_score+1;
+        // answer[1] = 6-min_score+1;
+        answer[0] = getRank(max_score);
+        answer[1] = getRank(min_score);
+        return answer;
+    }
+    
+    public int getRank(int num){
+        switch(num){
+            case 6:
+                return 1;
+            case 5:
+                return 2;
+            case 4:
+                return 3;
+            case 3:
+                return 4;
+            case 2:
+                return 5;
+            default:
+                return 6;
+        }
+        
+    }
+}
