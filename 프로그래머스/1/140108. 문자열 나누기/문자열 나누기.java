0bd512db7159ba31d7df32e219e1c1f21b74f31f
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-05-27 01:44:49 +0900

    [level 1] Title: 문자열 나누기, Time: 0.72 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,34 +1,53 @@
 class Solution {
     public int solution(String s) {
-        String word = s;
-        int answer = 0;
-
-        while(!word.isEmpty()){
-            char start = word.charAt(0); //시작 문자 추출 
-            int correct_cnt=0;
-            int not_correct_cnt=0;  
+//         String word = s;
+//         int answer = 0;
+//         while(!word.isEmpty()){
+//             char start = word.charAt(0); //시작 문자 추출 
+//             int correct_cnt=0;
+//             int not_correct_cnt=0;  
             
-            int i = 0;
-            for(; i<word.length(); i++){
-                if(word.charAt(i) == start){
-                    correct_cnt++;
-                }
-                else{
-                    not_correct_cnt++;
-                }
-                if(correct_cnt == not_correct_cnt){
-                    break;
-                }
-            }
+//             int i = 0;
+//             for(; i<word.length(); i++){
+//                 if(word.charAt(i) == start){
+//                     correct_cnt++;
+//                 }
+//                 else{
+//                     not_correct_cnt++;
+//                 }
+//                 if(correct_cnt == not_correct_cnt){
+//                     break;
+//                 }
+//             }
 
-            answer++;
-            if(i+1 < word.length()){
-                word = word.substring(i+1);    
-            }else{
-                break;
+//             answer++;
+//             if(i+1 < word.length()){
+//                 word = word.substring(i+1);    
+//             }else{
+//                 break;
+//             }
+//         }
+        
+        int answer = 0;
+        char init = s.charAt(0);
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (count == 0) {
+                init = c;
+            }
+            if (init == c) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                answer++;
             }
         }
-        
+
+        if(count > 0) {
+            answer++;
+        }
         return answer;
     }
 }
