427dc3276ea94474af9c14fc129536b87027677a
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-05-29 16:06:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 110.53 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -8,8 +8,11 @@ class Solution {
         
         //이름별로 참여자 map에 저장
         for(String runner : participant){
-            map.putIfAbsent(runner, 0);
-            map.put(runner, map.get(runner)+1);
+            // map.putIfAbsent(runner, 0);
+            // map.put(runner, map.get(runner)+1);
+            
+            //key가 존재한다면 value를 가져와서 +1, 존재하지 않는다면 1
+            map.put(runner, map.getOrDefault(runner, 0) + 1);
         }
         
         //완주자의 이름이 참여자에 있는지 확인
@@ -18,6 +21,7 @@ class Solution {
             map.put(finish, map.get(finish)-1);
         }
         
+        //value가 0이 아니면 아직 완주하지 못한 동명이인이 있음 
         for(String s : map.keySet()){
             if(map.get(s) >= 1){
                 answer+=s;
