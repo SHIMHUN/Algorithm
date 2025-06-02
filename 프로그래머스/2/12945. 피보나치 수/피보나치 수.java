fdae2117c8403c0ab8ec36af801b116797316463
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-06-02 21:43:41 +0900

    [level 2] Title: 피보나치 수, Time: 16.42 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,15 +1,18 @@
 class Solution {
-    static int DP[];
-    
+    static int[] memo;
+
     public int solution(int n) {
-        DP = new int[n+1];
-        DP[0] = 0;
-        DP[1] = 1;
-        
-        for(int i=2; i<=n; i++){
-            DP[i] = (DP[i-1] + DP[i-2]) % 1234567;
-        }
-        // return DP[n] % 1234567;
-        return DP[n];
+        memo = new int[n+1];
+        return fib(n);
+    }
+
+    private int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (memo[n] != 0) return memo[n]; // 이미 계산된 값이면 바로 반환
+
+        // 계산 후 저장(메모이제이션)
+        memo[n] = (fib(n-1) + fib(n-2)) % 1234567;
+        return memo[n];
     }
 }
