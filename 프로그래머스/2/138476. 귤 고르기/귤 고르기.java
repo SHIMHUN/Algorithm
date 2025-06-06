ff6476a8925865000f896cb5ef1b4e2447355519
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-06-06 16:38:19 +0900

    [level 2] Title: 귤 고르기, Time: 102.14 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    static Map<Integer, Integer> map = new HashMap<>();
+    
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        // map에 저장
+        for(int fruit : tangerine){
+            map.putIfAbsent(fruit, 0);
+            map.put(fruit, map.get(fruit)+1);
+        }
+        
+        // value 기준으로 내림차순 정렬
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort((o1, o2) -> o2.getValue() - o1.getValue());
+        
+        // k개를 만족할 때까지 누적
+        for(Map.Entry<Integer, Integer> entry : entryList){
+            int value = entry.getValue();
+            
+            k-=value;
+            answer++;
+            if(k <= 0){
+                break;
+            }
+            
+            // if(!(value > k) && k-value >= 0){
+            //     k-=value;
+            //     answer++;
+            //     if(k == 0){
+            //         break;
+            //     }
+            // }
+            
+        }
+        // System.out.println(map.toString());
+        
+        return answer;
+    }
+}
