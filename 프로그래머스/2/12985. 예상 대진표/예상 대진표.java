ea64318fdf9f1fd0e6662a996c57b00ab8d768b4
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-07-01 17:37:07 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,10 +2,10 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {   
-        int round = 0;
-        while (a != b) {
-            a = (a + 1) / 2;  // 다음 라운드 번호
-            b = (b + 1) / 2;
+        int round = 1;
+        while((a+1)/2 != (b+1)/2){
+            a = (a/2 + a%2);
+            b = (b/2 + b%2);
             round++;
         }
         
