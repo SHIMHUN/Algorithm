529d2a62c9337ab0e58058df77b462c012be10ed
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-07-02 22:32:30 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 238.00 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,34 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int[] elements) {
         Set<Integer> set = new HashSet<>();
+        int n = elements.length;
         
+        // 1. 배열 2배로 확장
+        int[] extended = new int[n*2];
+        for(int i=0; i<n*2; i++){
+            extended[i] = elements[i%n];
+        }
         
-//         for(int i=1; i<elements.length; i++){ // 0, 1, 2, 3 ... 연속 수열 최대 갯수
-//             for(int j=0; j<elements.length; j++){
-//                 int sum = 0;
-                
-//                 for(int z=j; z<i; z++){ 
-//                     sum += elements[(z+i)%elements.length];
-//                 }
-//                 System.out.println(i + " " + sum);
-//                 set.add(sum);
-//             }
-
-//         }
-        for(int z=1; z<=elements.length; z++){
-            for(int i=0; i<elements.length; i++){
-                int sum = 0;
-                for(int j=0; j<z; j++){ //오프셋
-                    sum += elements[(i+j)%elements.length];
-                    // System.out.println(z + " " + sum);
-
-                }
-                // System.out.println(z + " " + sum);
-                set.add(sum);
+        // 2. 누적 합 구해놓기
+        int[] sum = new int[2*n];
+        sum[0] = elements[0];
+        for(int i=1; i<2*n; i++){
+            sum[i] = sum[i-1] + extended[i];
+        }
+        
+        // 3. 시작, 끝 인덱스에 해당하는 누적 값 빼주기
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<n; j++){
+                int result = sum[j+i] - sum[i];
+                set.add(result);
             }
         }
 
-        // set.add()
         int answer = set.size();
         return answer;
     }
