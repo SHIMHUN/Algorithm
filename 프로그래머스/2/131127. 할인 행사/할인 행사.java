db28aa2bf0251fc4ca1a0adb378e4b22a453a467
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-07-03 18:08:58 +0900

    [level 2] Title: 할인 행사, Time: 54.79 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        Map<String, Integer> product = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        
+        // 상품 구성
+        for(int i=0; i<want.length; i++){
+            product.put(want[i],number[i]);
+        }
+        
+        // 초기 10일 간의 윈도우 설정
+        for(int i=0; i<10; i++){
+            map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
+        }
+        
+        int start = 0;
+        int end = 9;
+        int result = 0;
+        
+        
+        while(end < discount.length){
+            // 원하는 수량을 살 수 있는지 검사
+            boolean check = true;
+            for (String key : product.keySet()) {
+                if (map.getOrDefault(key, 0) != product.get(key)) {
+                    check = false;
+                    break;
+                }
+            }
+            // System.out.println(check);
+            if(check){
+                result++;
+            }
+            
+            // 4. 슬라이딩 윈도우 이동: map 업데이트
+            map.put(discount[start], map.get(discount[start]) - 1);
+            if (map.get(discount[start]) == 0) {
+                map.remove(discount[start]);
+            }
+
+            end++;
+            start++;
+
+            // 인덱스 체크 (discount.length 초과 방지)
+            if (end < discount.length) {
+                map.put(discount[end], map.getOrDefault(discount[end], 0) + 1);
+            }
+        }
+        
+        return result;
+    }
+}
