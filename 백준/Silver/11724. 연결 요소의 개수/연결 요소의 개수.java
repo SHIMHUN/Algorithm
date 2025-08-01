2858841a071ebaf57c2091ed6149b75ad0868e99
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-08-01 18:13:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 480 ms, Memory: 116464 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -22,14 +22,16 @@ public class Main {
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
 
+            //무방향 그래프
             graph[u][v] = 1;
             graph[v][u] = 1;
         }
 
         for(int i=1; i<N+1; i++){
-            if(!visited[i]){
-                dfs(i);
-                count++;
+            if(!visited[i]){ //방문하지않았으면 dfs 시작
+//                dfs(i);
+                bfs(i);
+                count++; //dfs 끝나면 연결요소 개수 +1
             }
         }
 
@@ -37,11 +39,28 @@ public class Main {
     }
 
     public static void dfs(int x){
-        visited[x] = true;
+        visited[x] = true; //현재 노드 방문 처리
 
         for(int i=1; i<=N; i++){
-            if(graph[x][i] == 1 && !visited[i]){
+            if(graph[x][i] == 1 && !visited[i]){ //값이 존재하며 아직 방문하지 않았으면 dfs 시작
                 dfs(i);
+            }
+        }
+    }
+
+    public static void bfs(int x){
+        visited[x] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(x);
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for(int i=1; i<=N; i++){
+                if(!visited[i] && graph[node][i] == 1){ //i는 가야할 곳, node는 현재 위치
+                    visited[i] = true;
+                    q.offer(i);
+                }
             }
         }
     }
