38d8c866a7339687590269909c8709cfd9cf11fb
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-09-08 15:26:41 +0900

    [level 2] Title: n^2 배열 자르기, Time: 20.42 ms, Memory: 98.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public List<Long> solution(int n, long left, long right) {
+        
+        List<Long> al = new ArrayList<>();
+        for(long i=left; i<=right; i++){
+            al.add(Math.max(i/n, i%n)+1);
+        }
+        return al;
+    }
+}
