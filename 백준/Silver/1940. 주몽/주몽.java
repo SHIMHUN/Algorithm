c3ccb00f3a7bede7966bfb90e73cf75617b67c81
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-09-17 22:38:51 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16432 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int start = 0;
+        int end = N-1;
+        int count = 0;
+
+        while(start < end){
+            if(A[start] + A[end] < M) {
+                start++;
+            }
+            else if(A[start] + A[end] > M){
+                end--;
+            }
+            else {
+                count++;
+                start++;
+                end--;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
