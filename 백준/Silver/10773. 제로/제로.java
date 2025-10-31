0e04ad0db4fb47c27a62614d88445cdc2abdeab2
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-11-01 00:35:07 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 22944 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,14 +1,18 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         Stack<Integer> stack = new Stack<>();
-        Scanner sc = new Scanner(System.in);
-        int k = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+//        Scanner sc = new Scanner(System.in);
+//        int k = sc.nextInt();
         int sum = 0;
 
         while(k-- > 0){
-            int num = sc.nextInt();
+//            int num = sc.nextInt();
+            int num = Integer.parseInt(br.readLine());
             if(num==0){
                 sum-=stack.pop();
             }else{
