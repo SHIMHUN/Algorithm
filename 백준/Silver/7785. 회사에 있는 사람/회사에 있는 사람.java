bb1f6b9c6d895604920c08f5e257a32200e9b06b
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2025-11-01 16:55:26 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 508 ms, Memory: 44760 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -11,9 +11,12 @@ public class Main {
         Set<String> set = new TreeSet<>(Collections.reverseOrder());
 
         while(n-- > 0){
-            String[] s = br.readLine().split(" ");
-            if(s[1].equals("enter")) set.add(s[0]);
-            else set.remove(s[0]);
+//            String[] s = br.readLine().split(" ");
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String cmd = st.nextToken();
+            if(cmd.equals("enter")) set.add(name);
+            else set.remove(name);
         }
 
 //        List<String> al = new ArrayList<>(set);
@@ -22,10 +25,11 @@ public class Main {
 //        for(String s : al){
 //            sb.append(s).append("\n");
 //        }
-        for(String s : set){
-            sb.append(s).append("\n");
-        }
+//        for(String s : set){
+//            sb.append(s).append("\n");
+//        }
 
+        set.forEach(s -> sb.append(s).append("\n"));
         System.out.print(sb);
     }
 }
