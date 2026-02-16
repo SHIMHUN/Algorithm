6666e3b71dc32b7c5e0fa5d6b8dec91f1ac3fd82
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2026-02-16 17:38:30 +0900

    [level 2] Title: 타겟 넘버, Time: 5.09 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,22 @@
 class Solution {
     static int count = 0;
+    int[] numbers;
+    int target;
     
-    public int solution(int[] numbers, int target) {        
-        dfs(numbers, target, 0, 0);
+    public int solution(int[] numbers, int target) {   
+        this.numbers=numbers;
+        this.target=target;
+        dfs(0, 0);
         
         return count;
     }
     
-    static void dfs(int[] numbers, int target, int sum, int idx){
+    public void dfs(int sum, int idx){
         if(idx==numbers.length){
             if(sum==target) count++;
             return;
         }
-        dfs(numbers, target, sum+numbers[idx], idx+1);
-        dfs(numbers, target, sum-numbers[idx], idx+1);
+        dfs(sum+numbers[idx], idx+1);
+        dfs(sum-numbers[idx], idx+1);
     }
 }
