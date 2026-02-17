8004f2a298281e6263693e74b0f0bd35dec3f918
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2026-02-17 18:37:54 +0900

    [level 2] Title: 주식가격, Time: 37.59 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[prices.length];
+        
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && 
+                   prices[i] < prices[stack.peek()]) {
+
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length - 1 - idx;
+        }
+        
+        return answer;
+    }
+}
